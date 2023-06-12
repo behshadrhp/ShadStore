95859c361a0f72bba7ff8894ee50daf2f89e0fc2
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-06-12 08:43:02 +0330

    create: account app

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # development
-    'store.apps.StoreConfig'
+    'store.apps.StoreConfig',
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [

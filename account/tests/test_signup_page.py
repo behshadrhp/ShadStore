09377ccfe810bacb5875cf9946469c2f12e86983
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-06-14 14:19:45 +0330

    create: TestSignUpPage at account.tests.test_signup_page

---
+++ account/tests/test_signup_page.py
@@ -0,0 +1,33 @@
+from django.urls import reverse, resolve
+from pytest_django.asserts import assertTemplateUsed, assertContains
+
+import pytest
+
+from account.forms import UserCreationForm
+from account.views import SignUpView
+
+class TestSignUpPage():
+    """This class is for test signup page template."""
+
+    @pytest.fixture
+    def signup(self, client):
+        url = reverse('signup')
+        response = client.get(url)
+        return response
+    
+    def test_signup_template(self, signup):
+        """This function is for test template response."""
+        assert signup.status_code == 200
+        assertTemplateUsed(signup, 'registration/signup.html')
+        assertContains(signup, 'Sign Up')
+
+    def test_signup_form(self, signup):
+        """This function is for test form registration."""
+        form = signup.context.get('form')
+        assert form.__class__ == UserCreationForm
+        assertContains(signup, 'csrfmiddlewaretoken')
+
+    def test_signup_view(self):
+        """This function is for signup url path."""
+        view = resolve('/account/signup/')
+        assert view.func.__name__ == SignUpView.as_view().__name__

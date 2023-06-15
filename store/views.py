67e56d363e876147782a47b747506d2e632aa127
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-06-15 21:51:00 +0330

    create: AboutView at store.views

--- store/views.py
+++ store/views.py
@@ -5,4 +5,10 @@ from django.shortcuts import render
 class HomeView(View):
     
     def get(self, request):
-        return render(request, "home.html")
+        return render(request, 'home.html')
+    
+
+class AboutView(View):
+
+    def get(self, request):
+        return render(request, 'about.html')

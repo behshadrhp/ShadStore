ed85e51623467a4425dcd575a4f948f66891f4c1
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-06-15 22:05:39 +0330

    create: test_about_page and write test into file

--- core/settings.py
+++ core/settings.py
@@ -105,7 +105,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 

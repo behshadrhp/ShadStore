ed5bb3cd1baa14d7e53ef30641c08fec96c7c1a6
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-06-17 16:15:41 +0330

    update: test_signup_page at accounts.tests.test_signup_page

--- accounts/tests/test_signup_page.py
+++ accounts/tests/test_signup_page.py
@@ -1,31 +1,37 @@
+from django.contrib.auth import get_user_model
 from django.urls import reverse, resolve
 from pytest_django.asserts import assertTemplateUsed, assertContains
 
 import pytest
 
-from accounts.forms import UserCreationForm
+
 from accounts.views import SignUpView
 
 class TestSignUpPage():
     """This class is for test signup page template."""
 
-    @pytest.fixture
-    def signup(self, client):
-        url = reverse('signup')
-        response = client.get(url)
-        return response
-    
-    def test_signup_template(self, signup):
-        """This function is for test template response."""
-        assert signup.status_code == 200
-        assertTemplateUsed(signup, 'registration/signup.html')
-        assertContains(signup, 'Sign Up')
+    username = "ted"
+    email = "ted@email.com"
 
-    def test_signup_form(self, signup):
+    @pytest.fixture
+    def response(self, client):
+        url = reverse('account_signup')
+        return client.get(url)
+    
+    @pytest.mark.django_db
+    def test_signup_template(self, response):
+        """This function is for test template response."""
+        assert response.status_code == 200
+        assertTemplateUsed(response, 'account/signup.html')
+        assertContains(response, 'Sign Up')
+
+    @pytest.mark.django_db
+    def test_signup_form(self):
         """This function is for test form registration."""
-        form = signup.context.get('form')
-        assert form.__class__ == UserCreationForm
-        assertContains(signup, 'csrfmiddlewaretoken')
+        user = get_user_model().objects.create_user(self.username, self.email)
+        assert get_user_model().objects.all().count() == 1
+        assert get_user_model().objects.all()[0].username == self.username
+        assert get_user_model().objects.all()[0].email == self.email
 
     def test_signup_view(self):
         """This function is for signup url path."""

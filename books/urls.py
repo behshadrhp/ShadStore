ad341ae009e3d8a384b1e6fd1f4a621026c0fdcc
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-06-24 10:40:32 +0330

    create: BookDetailVie & conf urls

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BookListView.as_view(), name='book-list'),    
+    path('', views.BookListView.as_view(), name='book-list'),
+    path('<int:pk>/', views.BookDetailView.as_view() , name='book-detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from . import models
 
@@ -9,3 +9,11 @@ class BookListView(ListView):
     model = models.Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
+
+
+class BookDetailView(DetailView):
+    """This class is for detail book view."""
+
+    model = models.Book
+    context_object_name = 'book'
+    template_name = 'books/book_detail.html'

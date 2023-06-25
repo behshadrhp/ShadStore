115c6a002faae82e938b6e6cdaa04a375cef9193
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-06-25 10:01:11 +0330

    update: settings and accounts.view

--- core/settings.py
+++ core/settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres-db',
+        'PORT': '5432',
     }
 }
 

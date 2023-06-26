a8c662eb4a6d38ca19534ca87bb2f4ec1cd93a9e
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-06-26 15:27:49 +0330

    create: Review model at books.models

--- books/models.py
+++ books/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
 import uuid
 
 
+User = get_user_model()
 
 class Book(models.Model):
     """This class is for save books."""
@@ -25,3 +27,28 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse("book-detail", kwargs={"pk": self.pk})
     
+
+class Review(models.Model):
+    """This class is for comment to under book post."""
+
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    book = models.ForeignKey(
+        Book, 
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.review

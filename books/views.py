4d9aa570b216c0f938219e184d8166decc6017fd
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-06-28 12:01:11 +0330

    create: PermissionRequiredMixin

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
 
 from . import models
@@ -13,10 +13,11 @@ class BookListView(LoginRequiredMixin, ListView):
     login_url = 'account_login'
 
 
-class BookDetailView(LoginRequiredMixin, DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     """This class is for detail book view."""
 
     model = models.Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
+    permission_required = 'books.special_status'

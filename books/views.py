c2898af4354d6f221ba623145321755551f13c0b
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-06-28 11:44:51 +0330

    apply: permissions for Book[List, Detail]View at books.views

--- books/views.py
+++ books/views.py
@@ -1,19 +1,22 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 
 from . import models
 
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     """This class is for list books."""
 
     model = models.Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
+    login_url = 'account_login'
 
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, DetailView):
     """This class is for detail book view."""
 
     model = models.Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
+    login_url = 'account_login'

e2ef7588b8b4ba00471419dc7e4939ab57f1af87
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-06-29 11:49:21 +0330

    create: SearchResultsListView & configure rooter at books.[views, urls]

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.BookListView.as_view(), name='book-list'),
     path('<uuid:pk>/', views.BookDetailView.as_view() , name='book-detail'),
+    path('search/', views.SearchResultsListView.as_view(), name='search-results'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 from . import models
 
@@ -21,3 +22,18 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    """This class is for return search results."""
+
+    model = models.Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        results = models.Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
+        return results

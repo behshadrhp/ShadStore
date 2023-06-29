743facfe2a34219dbf74d46bd4b19a544b1e7e0a
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-06-29 15:49:31 +0330

    update: cookie

--- core/settings/production.py
+++ core/settings/production.py
@@ -19,7 +19,12 @@ DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')),
 }
 
+# SSL & HSTS
 SECURE_SSL_REDIRECT = os.env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 SECURE_HSTS_SECONDS = os.env.int("DJANGO_SECURE_HSTS_SECONDS", default=2592000) # 30 days
 SECURE_HSTS_INCLUDE_SUBDOMAINS = os.env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS",default=True)
 SECURE_HSTS_PRELOAD = os.env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+
+# COOKIE
+SESSION_COOKIE_SECURE = os.env.bool("DJANGO_SESSION_COOKIE_SECURE", default=True)
+CSRF_COOKIE_SECURE = os.env.bool("DJANGO_CSRF_COOKIE_SECURE", default=True)
